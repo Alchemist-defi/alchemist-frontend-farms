370e90208d83c3f32831ee83db99e9d7c997234d
Harshad-rk <harshad.r.k.100@gmail.com>
2021-12-01 22:48:21 +0530

    update json

--- src/views/Nfts/components/NftList.tsx
+++ src/views/Nfts/components/NftList.tsx
@@ -15,8 +15,8 @@ const NftList = () => {
 
   // const totalPage = nfts.length / sizePerPage
 
-  const [Level, setLevel] = useState(0)
-  const [Breed, setBreed] = useState(0)
+  const [Level, setLevel] = useState('')
+  const [Breed, setBreed] = useState('')
   const [Claw, setClaw] = useState(null)
   const [Wingspan, setWingspan] = useState(null)
   const [Sight, setSight] = useState(null)
@@ -24,10 +24,10 @@ const NftList = () => {
   useEffect(() => {
     setNftData([])
     let mainData = nfts
-    if (Level > 0) {
+    if (Level != '') {
       mainData = mainData.filter((e) => e.level == Level)
     }
-    if (Breed > 0) {
+    if (Breed != '') {
       mainData = mainData.filter((e) => e.level == Level)
     }
     if (Claw) {
@@ -48,95 +48,104 @@ const NftList = () => {
   return (
     <>
       <div className="row mt-4">
-         <div className="col-12 col-md-2 mb-2"><Form.Select
-          aria-label="Default select example"
-          value={Level}
-          id="as"
-          onChange={(e) => {
-            setLevel(parseInt(e.target.value))
-          }}
-        >
-          <option value={0}>Level</option>
-          <option value={1}>One</option>
-          <option value={2}>Two</option>
-          <option value={3}>Three</option>
-          <option value={4}>Four</option>
-          <option value={5}>Five</option>
-        </Form.Select>
+        <div className="col-12 col-md-2 mb-2">
+          <Form.Select
+            aria-label="Default select example"
+            value={Level}
+            id="as"
+            onChange={(e) => {
+              setLevel(e.target.value)
+            }}
+          >
+            <option value={0}>Level</option>
+            <option value={1}>One</option>
+            <option value={2}>Two</option>
+            <option value={3}>Three</option>
+            <option value={4}>Four</option>
+            <option value={5}>Five</option>
+          </Form.Select>
         </div>
-         <div className="col-12 col-md-2 mb-2"><Form.Select
-
-          aria-label="Default select example"
-          value={Breed}
-          id="asads"
-          onChange={(e) => {
-            setBreed(parseInt(e.target.value))
-          }}
-        >
-          <option value={0}>Breed</option>
-          <option value={1}>One</option>
-          <option value={2}>Two</option>
-          <option value={3}>Three</option>
-          <option value={4}>Four</option>
-        </Form.Select>
-        </div>
-
-         <div className="col-12 col-md-2 mb-2"><Form.Select
-
-          aria-label="Default select example"
-          value={Claw}
-          id="asdasd"
-          onChange={(e) => {
-            setClaw(e.target.value)
-          }}
-        >
-          <option value="null">Claw</option>
-          <option value="LEGENDARY">LEGENDARY</option>
-          <option value="AVERAGE">AVERAGE</option>
-          <option value="MYSTICAL">MYSTICAL</option>
-        </Form.Select>
-        </div>
-
-         <div className="col-12 col-md-2 mb-2"><Form.Select
-
-          aria-label="Default select example"
-          value={Wingspan}
-          id="saddas"
-          onChange={(e) => {
-            setWingspan(e.target.value)
-          }}
-        >
-          <option value="null">Wingspan</option>
-          <option value="LEGENDARY">LEGENDARY</option>
-          <option value="AVERAGE">AVERAGE</option>
-          <option value="MYSTICAL">MYSTICAL</option>
-        </Form.Select>
+        <div className="col-12 col-md-2 mb-2">
+          <Form.Select
+            aria-label="Default select example"
+            value={Breed}
+            id="asads"
+            onChange={(e) => {
+              setBreed(e.target.value)
+            }}
+          >
+            <option value={0}>Breed</option>
+            <option value={1}>One</option>
+            <option value={2}>Two</option>
+            <option value={3}>Three</option>
+            <option value={4}>Four</option>
+          </Form.Select>
         </div>
 
         <div className="col-12 col-md-2 mb-2">
-        <Form.Select aria-label="Default select example"        
-
-          value={Sight}
-          id="adsas" onChange=
-          {(e) => {
-            setSight(e.target.value)
-          }}>
-          <option value="null">Sight</option>
-          <option value="LEGENDARY">LEGENDARY</option>
-          <option value="AVERAGE">AVERAGE</option>
-          <option value="MYSTICAL">MYSTICAL</option>
-        </Form.Select>
+          <Form.Select
+            aria-label="Default select example"
+            value={Claw}
+            id="asdasd"
+            onChange={(e) => {
+              setClaw(e.target.value)
+            }}
+          >
+            <option value="null">Claw</option>
+            <option value="LEGENDARY">LEGENDARY</option>
+            <option value="AVERAGE">AVERAGE</option>
+            <option value="MYSTICAL">MYSTICAL</option>
+          </Form.Select>
         </div>
 
+        <div className="col-12 col-md-2 mb-2">
+          <Form.Select
+            aria-label="Default select example"
+            value={Wingspan}
+            id="saddas"
+            onChange={(e) => {
+              setWingspan(e.target.value)
+            }}
+          >
+            <option value="null">Wingspan</option>
+            <option value="LEGENDARY">LEGENDARY</option>
+            <option value="AVERAGE">AVERAGE</option>
+            <option value="MYSTICAL">MYSTICAL</option>
+          </Form.Select>
+        </div>
+
+        <div className="col-12 col-md-2 mb-2">
+          <Form.Select
+            aria-label="Default select example"
+            value={Sight}
+            id="adsas"
+            onChange={(e) => {
+              setSight(e.target.value)
+            }}
+          >
+            <option value="null">Sight</option>
+            <option value="LEGENDARY">LEGENDARY</option>
+            <option value="AVERAGE">AVERAGE</option>
+            <option value="MYSTICAL">MYSTICAL</option>
+          </Form.Select>
+        </div>
       </div>
-      <NftGrid>
-        {nftData.map((element, index) => (
-          <div key={index}>
-            <NftCard nft={element} index={index} type="ALL" />
-          </div>
-        ))}
-      </NftGrid>
-      <Pagination current={page} total={totalPage + 1} onPageChange={setPage} />
+      <div className="row mt-4">
+        <div className="col-12 col-md-12 mb-12">
+          <NftGrid>
+            {nftData.map((element, index) => (
+              <div key={index}>
+                <NftCard nft={element} index={index} type="ALL" />
+              </div>
+            ))}
+          </NftGrid>
+        </div>
+      </div>
+      <div className="row mt-4">
+        <div className="col-12 col-md-12 mb-12">
+          <Pagination current={page} total={totalPage + 1} onPageChange={setPage} />
+        </div>
+      </div>
     </>
   )
 }
